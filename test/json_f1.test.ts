56e4fdb660f0aee86ad209f2032c86f9c41308f3
nvelandia <nvelandia97@gmail.com>
2024-12-03 18:18:42 -0300

    Initial commit

---
+++ test/json_f1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JsonF1 from '../lib/json_f1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/json_f1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JsonF1.JsonF1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
